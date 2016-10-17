3df0a83cda8b99f97f1687504be78d4898b894ed
Sardor Muminov <sardor@insilicogen.com>
2016-10-17 18:18:49 +0900

    Initial untested prototype

--- tasix/middleware.py
+++ tasix/middleware.py
@@ -1,8 +1,16 @@
-def simple_middleware(get_response):
+# Copyright 2016 django-tasix authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
 
-    def middleware(request):
-        response = get_response(request)
+from django.conf import settings
+from django.http import HttpResponseForbidden
+from tasix.network import is_tasix_member
 
-        return response
 
-    return middleware
+class TasixMiddleware(object):
+
+    def process_request(self, request):
+        ip_address = request.META['REMOTE_ADDR']
+        allow_tasix_only = getattr(settings, "ALLOW_TASIX", True)
+        if allow_tasix and not is_tasix_member(ip_address):
+            return HttpResponseForbidden
